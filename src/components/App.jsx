7528119f370c41172ed64bc98501cc2d5eed69df
jienhua <jien8013@hotmail.com>
2016-10-14 18:01:09 -0700

    save process

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,13 @@ import React, {Component} from 'react'
 import {Link} from 'react-router'
 
 import Header from './Header/Header'
+import Navbar from './Navbar/Navbar'
 
 class App extends Component{
 	render() {
 		return (
 			<div>
+				<Navbar/>
 				<Header/>
 
 				{this.props.children}
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,10 @@
 import React, {Component} from 'react'
 
-import Navbar from '../Navbar/Navbar'
-
 require('./Header.css')
 
 const header = (
 	<div id='header'>
 		<h1>header1</h1>
-		<Navbar/>
 	</div>
 )
 
