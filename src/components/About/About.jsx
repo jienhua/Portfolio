27e94db8578a4a0c901e66a3fbfcbd8aaea62e9b
jienhua <jien8013@hotmail.com>
2016-10-19 17:37:27 -0700

    save process

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-
+import {Grid, Row, Col} from 'react-bootstrap'
 require('./About.css')
 
 class About extends Component{
@@ -8,14 +8,23 @@ class About extends Component{
 			<div className='container'>
 				<h1>about test</h1>
 				<hr/>
-				My name is {this.props.data.about.basicInfo.name}.<br/>
-				I am a {this.props.data.about.basicInfo.title}.<br/><br/>
-				My favorite programming language is {this.props.data.about.basicInfo.favProgramLang}<br/>
-				Skills<br/>
-					{this.props.data.about.basicInfo.skills.map((skill, index) =>{
-						return <spam key={index} className='skill'> <small>#{skill}</small> </spam>
-					})}
-
+				<Grid>
+					<Row>
+						<Col xs={6}>
+							My name is {this.props.data.about.basicInfo.name}.<br/>
+							I am a {this.props.data.about.basicInfo.title}.<br/><br/>
+							My favorite programming language is {this.props.data.about.basicInfo.favProgramLang}<br/>
+							Skills<br/>
+							{this.props.data.about.basicInfo.skills.map((skill, index) =>{
+								return <spam key={index} className='skill'> <small>#{skill}</small> </spam>
+							})}
+						</Col>
+						<Col xs={6}>
+							right
+						</Col>
+					</Row>
+				</Grid>
+				
 			</div>
 		)
 	}
