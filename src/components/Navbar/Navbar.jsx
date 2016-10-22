669ac3b5d41b4d50b7ddfd0ad7715232165beb04
jienhua <jien8013@hotmail.com>
2016-10-21 17:07:58 -0700

    navbar now collapse

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,28 +1,32 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router'
-import { Nav, Navbar, NavbarBrand, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
+import { Nav, Navbar, NavbarBrand, NavItem, NavDropdown, MenuItem, Collapse, Toggle} from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
 require('./Navbar.css')
 
 const navbar = (
-  <Navbar fixedTop>
+  <Navbar fixedTop >
     <Navbar.Header>
-      <LinkContainer to='/portfolio'>
+      <LinkContainer to='/portfolio' >
         <Navbar.Brand>JIEN HUA</Navbar.Brand>
       </LinkContainer>
+      <Navbar.Toggle>
+      </Navbar.Toggle>
     </Navbar.Header>
-    <Nav pullRight>
-      <LinkContainer to='/portfolio'>
-    		<NavItem eventKey={1}>PORTFOLIO</NavItem>
-    	</LinkContainer>
-    	<LinkContainer to='/about'>
-    		<NavItem eventKey={2}>ABOUT</NavItem>
-    	</LinkContainer>
-    	<LinkContainer to='/experience'>
-    		<NavItem eventKey={3}>EXPERIENCE</NavItem>
-    	</LinkContainer>
-    </Nav>
+    <Navbar.Collapse>
+      <Nav pullRight >
+        <LinkContainer to='/portfolio'>
+      		<NavItem eventKey={1}>PORTFOLIO</NavItem>
+      	</LinkContainer>
+      	<LinkContainer to='/about'>
+      		<NavItem eventKey={2}>ABOUT</NavItem>
+      	</LinkContainer>
+      	<LinkContainer to='/experience'>
+      		<NavItem eventKey={3}>EXPERIENCE</NavItem>
+      	</LinkContainer>
+      </Nav>
+    </Navbar.Collapse>
   </Navbar>
 )
 
--- src/index.jsx
+++ src/index.jsx
@@ -13,8 +13,9 @@ const routes = 	<Route path='/' component={App}>
 					<IndexRoute component={Portfolio} />
 					<Route path='/about' component={About} />
 					<Route path='/experience' component={Experience} />
-					<Route path='/portfolio' component={Portfolio} />
-					<Route path='/portfolio/:projectName' component={Project}/>
+					<Route path='/portfolio' component={Portfolio}>
+						<Route path='/portfolio/:projectName' component={Project}/>
+					</Route>
 					<Route path='*' component={Portfolio}/>
 				</Route>
 
